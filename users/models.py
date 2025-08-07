fcbd532374c31a784adf8d53f43fdaa6ece35964
Rejina <rejinadangal4@gamil.com>
2025-08-07 21:19:12 +0545

    permission for user

--- users/models.py
+++ users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 def generateImagePath( instance, file):
-    return f'images/users/{instance.username}/{file}'
+    return f'images/users/{instance.user.username}/{file}'
 class Profile(models.Model):
     class RoleOptions(models.TextChoices):
         Employer= 'E', 'Employer'
--- users/viewsets.py
+++ users/viewsets.py
@@ -2,11 +2,14 @@ from rest_framework import viewsets
 from .models import Profile
 from .serializers import ProfileSerializer, UserSerializer
 from django.contrib.auth.models import User
+from .permissions import *
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsUserOwnerOrGetPost,]
     
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+    permission_classes = [IsProfileOwnerOrGetPost,]
